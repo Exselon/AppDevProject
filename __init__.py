8abd6fb48fab9fa8d52b7a32482e883f9927cf4f
exselon <johnathanlimmm@gmail.com>
2023-11-01 11:42:36 +0800

    update

--- __init__.py
+++ __init__.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/contactUs')
+def contact_us():
+    return render_template('contactUs.html')
+
 if __name__ == '__main__':
     app.run()
