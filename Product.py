c9d97f481d14d5826ddffae3a24f36d6918d336d
Chien <johnathanlimmm@gmail.com>
2023-12-17 03:20:04 +0800

    Product Update

--- Product.py
+++ Product.py
@@ -15,3 +15,23 @@ class ProductManager:
         self.conn = sqlite3.connect(db_file)
         self.cursor = self.conn.cursor()
 
+    def get_all_products(self):
+        self.cursor.execute("SELECT * FROM products")
+        products_data = self.cursor.fetchall()
+        products = [Product(*data) for data in products_data]
+        return products
+
+    def get_product_by_id(self, product_id):
+        self.cursor.execute("SELECT * FROM products WHERE id=?", (product_id,))
+        product_data = self.cursor.fetchone()
+        if product_data:
+            return Product(*product_data)
+        return None
+
+    def add_product(self, name, price, image_path):
+        self.cursor.execute("INSERT INTO products (name, price, image) VALUES (?, ?, ?)",
+                            (name, price, image_path))
+        self.conn.commit()
+
+    def close_connection(self):
+        self.conn.close()
